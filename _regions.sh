d7c1cd372281c47236875d3a8ee0727c482efdda
Huevos y Bacon <Huevos-y-Bacon@users.noreply.github.com>
2019-06-11 16:56:55 +0100

    Update _regions.sh
    reverse selection. i.e. delete default VPCs everywhere but one

--- _regions.sh
+++ _regions.sh
@@ -4,20 +4,20 @@
 
 # All normal regions as on 11 Jun 19:
 declare -a REGION_ARR=(
-    eu-west-1
-    # eu-west-2
-    # eu-west-3
-    # eu-central-1
-    # eu-north-1
+    # eu-west-1
+    eu-west-2
+    eu-west-3
+    eu-central-1
+    eu-north-1
     us-east-1
-    # us-east-2
-    # us-west-1
-    # us-west-2
-    # ca-central-1
-    # sa-east-1
-    # ap-northeast-1
-    # ap-northeast-2
-    # ap-south-1
-    # ap-southeast-1
-    # ap-southeast-2
+    us-east-2
+    us-west-1
+    us-west-2
+    ca-central-1
+    sa-east-1
+    ap-northeast-1
+    ap-northeast-2
+    ap-south-1
+    ap-southeast-1
+    ap-southeast-2
 )
